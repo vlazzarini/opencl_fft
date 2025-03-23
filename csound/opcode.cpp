c434ddcca4d740a9c2ad2e9728e71b752708e978
vlazzarini <victor.lazzarini@mu.ie>
2025-03-23 12:19:40 +0000

    added cl_fft opcode

--- csound/opcode.cpp
+++ csound/opcode.cpp
@@ -22,205 +22,272 @@
 
 #include <cl_conv.h>
 #include <cl_dconv.h>
+#include <cl_fft.h>
 #include <modload.h>
 #include <vector>
 
 namespace csnd {
-void err_msg(std::string s, void *uData) {
-  Csound *cs = (Csound *)uData;
-  cs->message(s);
-}
+  static inline uint32_t np2(uint32_t n) {
+    uint32_t v = 2;
+    while (v < n)
+       v <<= 1;
+    return v;
+  } 
 
-struct Conv : Plugin<1, 6> {
-  cl_conv::Clpconv *clpconv;
-  cl_conv::Cldconv *cldconv;
-  Table ir;
-  int parts, cnt;
-  bool dconv;
-  csnd::AuxMem<float> bufin, bufout;
+  
+  void err_msg(std::string s, void *uData) {
+    Csound *cs = (Csound *)uData;
+    cs->message(s);
+  }
 
-  int init() {
-    int size;
-    int err;
-    cl_device_id device_ids[32], id;
-    cl_uint num = 0, nump = 0;
-    char name[128];
+  struct Cfft : Plugin<1,3> {
+    
+    cl_fft::Clcfft *dft;
+    csnd::AuxMem<float> buf;
+    
+    int init() {
+        int err;
+        cl_device_id device_ids[32], id;
+        cl_uint num = 0;
+        char name[128];
+        Vector<MYFLT> input = inargs.vector_data<MYFLT>(0);
+        Vector<MYFLT> output = outargs.vector_data<MYFLT>(0);
+        output.init(csound, input.len(), this->insdshead);
 
-    err = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_ALL, 32, device_ids, &num);
-    if (err != CL_SUCCESS)
-      return csound->init_error("failed to find an OpenCL device!\n");
-    id = device_ids[(int)inargs[3]];
-    clGetDeviceInfo(id, CL_DEVICE_NAME, 128, name, NULL);
-    csound->message("using device: ");
-    csound->message(name);
-    ir.init(csound, inargs(1));
-    parts = inargs[2];
-    size = inargs[5] == 0 ? ir.len() : inargs[5];
-    size -= inargs[4];
-    MYFLT _0dbfs = csound->_0dbfs();
-    dconv = parts == 1 ? true : false;
-    if (dconv) {
-      int ksmps = insdshead->ksmps;
-      cldconv = new cl_conv::Cldconv(id, size, ksmps, err_msg, (void *)csound);
-      if (cldconv->get_cl_err() == CL_SUCCESS) {
-        std::vector<float> coefs(size);
-        for (int i = 0; i < size; i++)
-          coefs[i] = ir[i] * _0dbfs;
-        if (cldconv->push_ir(coefs.data()) == CL_SUCCESS) {
+        err = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_ALL, 32, device_ids, &num);
+        if (err != CL_SUCCESS)
+          return csound->init_error("failed to find an OpenCL device!\n");
+        id = device_ids[(int)inargs[2]];
+        clGetDeviceInfo(id, CL_DEVICE_NAME, 128, name, NULL);
+        csound->message("using device: ");
+        csound->message(name);
+        dft = new cl_fft::Clcfft(id, np2(input.len()), inargs[1] ? true : false);
+        if ((err = dft->get_error()) > 0) 
+          return csound->init_error(cl_fft::cl_error_string(err));
+        buf.allocate(csound, input.len());
+        return OK;
+     }
+
+     int perf() {
+        int err;
+        Vector<MYFLT> input = inargs.vector_data<MYFLT>(0);
+        Vector<MYFLT> output = outargs.vector_data<MYFLT>(0);
+        std::complex<float> *data =
+          reinterpret_cast<std::complex<float>*>(buf.data());
+
+        for(auto s : input)
+          buf[i] = s;
+        
+        err = dft->transform(data);
+
+        if ((err = dft->get_error()) > 0) 
+          return csound->perf_error(cl_fft::cl_error_string(err), this);
+
+         for(auto &s : output)
+           s = buf[i];
+    
+        return OK;
+      }
+
+      int deinit() {
+        delete dft;
+        return OK;
+      }
+
+  };
+
+  
+  struct Conv : Plugin<1, 6> {
+    cl_conv::Clpconv *clpconv;
+    cl_conv::Cldconv *cldconv;
+    Table ir;
+    int parts, cnt;
+    bool dconv;
+    csnd::AuxMem<float> bufin, bufout;
+
+    int init() {
+      int size;
+      int err;
+      cl_device_id device_ids[32], id;
+      cl_uint num = 0, nump = 0;
+      char name[128];
+
+      err = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_ALL, 32, device_ids, &num);
+      if (err != CL_SUCCESS)
+        return csound->init_error("failed to find an OpenCL device!\n");
+      id = device_ids[(int)inargs[3]];
+      clGetDeviceInfo(id, CL_DEVICE_NAME, 128, name, NULL);
+      csound->message("using device: ");
+      csound->message(name);
+      ir.init(csound, inargs(1));
+      parts = inargs[2];
+      size = inargs[5] == 0 ? ir.len() : inargs[5];
+      size -= inargs[4];
+      MYFLT _0dbfs = csound->_0dbfs();
+      dconv = parts == 1 ? true : false;
+      if (dconv) {
+        int ksmps = insdshead->ksmps;
+        cldconv = new cl_conv::Cldconv(id, size, ksmps, err_msg, (void *)csound);
+        if (cldconv->get_cl_err() == CL_SUCCESS) {
+          std::vector<float> coefs(size);
+          for (int i = 0; i < size; i++)
+            coefs[i] = ir[i] * _0dbfs;
+          if (cldconv->push_ir(coefs.data()) == CL_SUCCESS) {
+            bufout.allocate(csound, ksmps);
+            bufin.allocate(csound, ksmps);
+            cnt = 0;
+            return OK;
+          }
+          csound->message("error setting impulse response");
+        }
+        delete cldconv;
+      } else {
+        clpconv = new cl_conv::Clpconv(id, size, parts, err_msg, (void *)csound);
+        if (clpconv->get_cl_err() == CL_SUCCESS) {
+          std::vector<float> coefs(size);
+          for (int i = 0; i < size; i++)
+            coefs[i] = ir[i] * _0dbfs;
+          if (clpconv->push_ir(coefs.data()) == CL_SUCCESS) {
+            bufout.allocate(csound, parts);
+            bufin.allocate(csound, parts);
+            cnt = 0;
+            return OK;
+          }
+          csound->message("error setting impulse response");
+        }
+        delete clpconv;
+      }
+      return csound->init_error("error initialising opencl object");
+    }
+
+    int deinit() {
+      if (dconv)
+        delete cldconv;
+      else
+        delete clpconv;
+      return OK;
+    }
+
+    int aperf() {
+      AudioSig asig(this, inargs(0));
+      AudioSig aout(this, outargs(0));
+
+      if (dconv) {
+        for (int n = offset; n < nsmps; n++)
+          bufin[n] = (float)asig[n];
+        if (cldconv->convolution(bufout.data(), bufin.data()) != CL_SUCCESS)
+          return csound->perf_error("error computing convolution\n", this);
+        for (int n = offset; n < nsmps; n++)
+          aout[n] = (MYFLT)bufout[n];
+      } else {
+        for (int n = offset; n < nsmps; n++) {
+          bufin[cnt] = (float)asig[n];
+          aout[n] = (MYFLT)bufout[cnt];
+          if (++cnt == parts) {
+            if (clpconv->convolution(bufout.data(), bufin.data()) != CL_SUCCESS)
+              return csound->perf_error("error computing convolution\n", this);
+            cnt = 0;
+          }
+        }
+      }
+      return OK;
+    }
+  };
+
+  struct TVConv : Plugin<1, 7> {
+    cl_conv::Clpconv *clpconv;
+    cl_conv::Cldconv *cldconv;
+    int parts, cnt;
+    bool dconv;
+    csnd::AuxMem<float> bufin1, bufin2, bufout;
+
+    int init() {
+      int size;
+      int err;
+      cl_device_id device_ids[32], id;
+      cl_uint num = 0, nump = 0;
+      char name[128];
+
+      err = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_ALL, 32, device_ids, &num);
+      if (err != CL_SUCCESS)
+        return csound->init_error("failed to find an OpenCL device!\n");
+      id = device_ids[(int)inargs[6]];
+      clGetDeviceInfo(id, CL_DEVICE_NAME, 128, name, NULL);
+      csound->message("using device: ");
+      csound->message(name);
+      size = inargs[5];
+      parts = inargs[4];
+      dconv = parts == 1 ? true : false;
+      if (dconv) {
+        int ksmps = insdshead->ksmps;
+        cldconv = new cl_conv::Cldconv(id, size, ksmps, err_msg, (void *)csound);
+        if (cldconv->get_cl_err() == CL_SUCCESS) {
           bufout.allocate(csound, ksmps);
-          bufin.allocate(csound, ksmps);
+          bufin1.allocate(csound, ksmps);
+          bufin2.allocate(csound, ksmps);
           cnt = 0;
           return OK;
         }
-        csound->message("error setting impulse response");
-      }
-      delete cldconv;
-    } else {
-      clpconv = new cl_conv::Clpconv(id, size, parts, err_msg, (void *)csound);
-      if (clpconv->get_cl_err() == CL_SUCCESS) {
-        std::vector<float> coefs(size);
-        for (int i = 0; i < size; i++)
-          coefs[i] = ir[i] * _0dbfs;
-        if (clpconv->push_ir(coefs.data()) == CL_SUCCESS) {
+        delete cldconv;
+      } else {
+        clpconv = new cl_conv::Clpconv(id, size, parts, err_msg, (void *)csound);
+        if (clpconv->get_cl_err() == CL_SUCCESS) {
+          cnt = 0;
           bufout.allocate(csound, parts);
-          bufin.allocate(csound, parts);
-          cnt = 0;
+          bufin1.allocate(csound, parts);
+          bufin2.allocate(csound, parts);
           return OK;
         }
-        csound->message("error setting impulse response");
+        delete clpconv;
       }
-      delete clpconv;
+      return csound->init_error("error initialising opencl object");
     }
-    return csound->init_error("error initialising opencl object");
-  }
 
-  int deinit() {
-    if (dconv)
-      delete cldconv;
-    else
-      delete clpconv;
-    return OK;
-  }
+    int deinit() {
+      if (dconv)
+        delete cldconv;
+      else
+        delete clpconv;
+      return OK;
+    }
 
-  int aperf() {
-    AudioSig asig(this, inargs(0));
-    AudioSig aout(this, outargs(0));
+    int aperf() {
+      AudioSig asig1(this, inargs(0));
+      AudioSig asig2(this, inargs(1));
+      AudioSig aout(this, outargs(0));
+      int frz1 = (int)inargs[2], frz2 = (int)inargs[2];
+      MYFLT _0dbfs = csound->_0dbfs();
 
-    if (dconv) {
-      for (int n = offset; n < nsmps; n++)
-        bufin[n] = (float)asig[n];
-      if (cldconv->convolution(bufout.data(), bufin.data()) != CL_SUCCESS)
-        return csound->perf_error("error computing convolution\n", this);
-      for (int n = offset; n < nsmps; n++)
-        aout[n] = (MYFLT)bufout[n];
-    } else {
-      for (int n = offset; n < nsmps; n++) {
-        bufin[cnt] = (float)asig[n];
-        aout[n] = (MYFLT)bufout[cnt];
-        if (++cnt == parts) {
-          if (clpconv->convolution(bufout.data(), bufin.data()) != CL_SUCCESS)
-            return csound->perf_error("error computing convolution\n", this);
-          cnt = 0;
+      if (dconv) {
+        for (int n = offset; n < nsmps; n++) {
+          bufin1[n] = (float)frz1 ? (float)(asig1[n] / _0dbfs) : bufin1[cnt];
+          bufin2[n] = (float)frz1 ? (float)(asig2[n] / _0dbfs) : bufin2[cnt];
+        }
+        if (cldconv->convolution(bufout.data(), bufin1.data(), bufin2.data()) !=
+            CL_SUCCESS)
+          return csound->perf_error("error computing convolution\n", this);
+        for (int n = offset; n < nsmps; n++)
+          aout[n] = bufout[n] * _0dbfs;
+      } else {
+        for (int n = offset; n < nsmps; n++) {
+          bufin1[cnt] = frz1 ? (float)(asig1[n] / _0dbfs) : bufin1[cnt];
+          bufin2[cnt] = frz1 ? (float)(asig2[n] / _0dbfs) : bufin2[cnt];
+          aout[n] = bufout[cnt] * _0dbfs;
+          if (++cnt == parts) {
+            if (clpconv->convolution(bufout.data(), bufin1.data(),
+                                     bufin2.data()) != CL_SUCCESS)
+              return csound->perf_error("error computing convolution\n", this);
+            cnt = 0;
+          }
         }
       }
+      return OK;
     }
-    return OK;
+  };
+
+  void on_load(Csound *csound) {
+    plugin<Conv>(csound, "clconv", "a", "aiiioo", csnd::thread::ia);
+    plugin<TVConv>(csound, "cltvconv", "a", "aakkiii", csnd::thread::ia);
+    plugin<Cfft>(csound, "clfft", "k[]", "k[]ii", csnd::thread::ik);
   }
-};
-
-struct TVConv : Plugin<1, 7> {
-  cl_conv::Clpconv *clpconv;
-  cl_conv::Cldconv *cldconv;
-  int parts, cnt;
-  bool dconv;
-  csnd::AuxMem<float> bufin1, bufin2, bufout;
-
-  int init() {
-    int size;
-    int err;
-    cl_device_id device_ids[32], id;
-    cl_uint num = 0, nump = 0;
-    char name[128];
-
-    err = clGetDeviceIDs(NULL, CL_DEVICE_TYPE_ALL, 32, device_ids, &num);
-    if (err != CL_SUCCESS)
-      return csound->init_error("failed to find an OpenCL device!\n");
-    id = device_ids[(int)inargs[6]];
-    clGetDeviceInfo(id, CL_DEVICE_NAME, 128, name, NULL);
-    csound->message("using device: ");
-    csound->message(name);
-    size = inargs[5];
-    parts = inargs[4];
-    dconv = parts == 1 ? true : false;
-    if (dconv) {
-      int ksmps = insdshead->ksmps;
-      cldconv = new cl_conv::Cldconv(id, size, ksmps, err_msg, (void *)csound);
-      if (cldconv->get_cl_err() == CL_SUCCESS) {
-        bufout.allocate(csound, ksmps);
-        bufin1.allocate(csound, ksmps);
-        bufin2.allocate(csound, ksmps);
-        cnt = 0;
-        return OK;
-      }
-      delete cldconv;
-    } else {
-      clpconv = new cl_conv::Clpconv(id, size, parts, err_msg, (void *)csound);
-      if (clpconv->get_cl_err() == CL_SUCCESS) {
-        cnt = 0;
-        bufout.allocate(csound, parts);
-        bufin1.allocate(csound, parts);
-        bufin2.allocate(csound, parts);
-        return OK;
-      }
-      delete clpconv;
-    }
-    return csound->init_error("error initialising opencl object");
-  }
-
-  int deinit() {
-    if (dconv)
-      delete cldconv;
-    else
-      delete clpconv;
-    return OK;
-  }
-
-  int aperf() {
-    AudioSig asig1(this, inargs(0));
-    AudioSig asig2(this, inargs(1));
-    AudioSig aout(this, outargs(0));
-    int frz1 = (int)inargs[2], frz2 = (int)inargs[2];
-    MYFLT _0dbfs = csound->_0dbfs();
-
-    if (dconv) {
-      for (int n = offset; n < nsmps; n++) {
-        bufin1[n] = (float)frz1 ? (float)(asig1[n] / _0dbfs) : bufin1[cnt];
-        bufin2[n] = (float)frz1 ? (float)(asig2[n] / _0dbfs) : bufin2[cnt];
-      }
-      if (cldconv->convolution(bufout.data(), bufin1.data(), bufin2.data()) !=
-          CL_SUCCESS)
-        return csound->perf_error("error computing convolution\n", this);
-      for (int n = offset; n < nsmps; n++)
-        aout[n] = bufout[n] * _0dbfs;
-    } else {
-      for (int n = offset; n < nsmps; n++) {
-        bufin1[cnt] = frz1 ? (float)(asig1[n] / _0dbfs) : bufin1[cnt];
-        bufin2[cnt] = frz1 ? (float)(asig2[n] / _0dbfs) : bufin2[cnt];
-        aout[n] = bufout[cnt] * _0dbfs;
-        if (++cnt == parts) {
-          if (clpconv->convolution(bufout.data(), bufin1.data(),
-                                   bufin2.data()) != CL_SUCCESS)
-            return csound->perf_error("error computing convolution\n", this);
-          cnt = 0;
-        }
-      }
-    }
-    return OK;
-  }
-};
-
-void on_load(Csound *csound) {
-  plugin<Conv>(csound, "clconv", "a", "aiiioo", csnd::thread::ia);
-  plugin<TVConv>(csound, "cltvconv", "a", "aakkiii", csnd::thread::ia);
-}
 }
